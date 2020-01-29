6baf7ad6e23a54b85886cc91ae4f8e2212d07fda
zhangyy <1002726018@qq.com>
2020-01-29 13:40:19 +0800

    springCloud 工程---仅创建了 eurake server和client

--- forum/src/main/java/com/dream/forum/ForumApplication.java
+++ forum/src/main/java/com/dream/forum/ForumApplication.java
@@ -16,17 +16,17 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Slf4j
 public class ForumApplication {
 
-    @Before
-    public void setUp() {
-    }
-    @Test
-    public void test() {
-        log.trace("trace 成功了");
-        log.debug("debug 成功了");
-        log.info("info 成功了");
-        log.warn("warn 成功了");
-        log.error("error 成功了");
-    }
+//    @Before
+//    public void setUp() {
+//    }
+//    @Test
+//    public void test() {
+//        log.trace("trace 成功了");
+//        log.debug("debug 成功了");
+//        log.info("info 成功了");
+//        log.warn("warn 成功了");
+//        log.error("error 成功了");
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(ForumApplication.class, args);
---
+++ springCloud/eurekaClient/src/test/java/com/demo/eurekaclient/EurekaclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.eurekaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
